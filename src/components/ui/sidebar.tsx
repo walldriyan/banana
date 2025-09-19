4b44512734fe99c1a10c74fc8e0fa244cbf76325
Pawani Kaushika <pawani.kaushika1984@gmail.com>
2025-09-19 18:22:10 +0000

    mini side bar ekak nane (_for element <Sidebar>_)

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -373,7 +373,7 @@ const SidebarFooter = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="footer"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2 p-2 mt-auto", className)}
       {...props}
     />
   )
