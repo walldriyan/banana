b6564c9271e9b2152046484b8babf7740aa204c7
Pawani Kaushika <pawani.kaushika1984@gmail.com>
2025-09-19 18:29:20 +0000

    tawamat icon vitarak tiyana mini side bar ekakk nane mata dewani image e

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -226,7 +226,7 @@ const Sidebar = React.forwardRef<
           className={cn(
             "duration-200 relative h-svh w-[--sidebar-width] bg-transparent transition-[width] ease-linear",
             "group-data-[collapsible=offcanvas]:w-0",
-            "group-data-[collapsible=icon]:w-[--sidebar-width-icon]", // Corrected location for the spacer div
+            "group-data-[collapsible=icon]:w-[--sidebar-width-icon]",
             "group-data-[side=right]:rotate-180",
             variant === "floating" || variant === "inset"
               ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4))]"
@@ -236,7 +236,7 @@ const Sidebar = React.forwardRef<
         <div
           className={cn(
             "duration-200 fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] transition-[left,right,width] ease-linear md:flex",
-            "group-data-[collapsible=icon]:w-[--sidebar-width-icon]", // This is the correct div to apply the collapsed width to
+            "group-data-[collapsible=icon]:w-[--sidebar-width-icon]",
             side === "left"
               ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
               : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
