8e8c03c44da3358f54ecdb207a702e475d833fba
Pawani Kaushika <pawani.kaushika1984@gmail.com>
2025-11-12 16:41:36 +0000

    me row overflow ynwane eka hadanna  (_for element <TableCell>_)

--- src/app/dashboard/products/data-table.tsx
+++ src/app/dashboard/products/data-table.tsx
@@ -113,7 +113,7 @@ export function ProductsDataTable<TData, TValue>({
                 </AuthorizationGuard>
             </div>
         </div>
-        <div className="rounded-md border flex-grow overflow-y-auto">
+        <div className="rounded-md border flex-grow overflow-auto">
             <Table>
                 <TableHeader>
                 {table.getHeaderGroups().map((headerGroup) => (
