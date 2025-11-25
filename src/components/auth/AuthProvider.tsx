f8413390b139404b4cfa2060cbdcfcb873139185
Pawani Kaushika <pawani.kaushika1984@gmail.com>
2025-11-25 15:40:59 +0000

    ## Error Type Console Error
    ## Error Message
    [next-auth][error][CLIENT_

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -11,7 +11,7 @@ export default function AuthProvider({
   children: React.ReactNode;
 }) {
   return (
-    <SessionProvider>
+    <SessionProvider basePath="/api/auth">
       <SessionUpdater />
       {children}
     </SessionProvider>
